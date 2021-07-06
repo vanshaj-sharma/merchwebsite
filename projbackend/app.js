ce6169e9455db16196e637d09de5da70556baca4
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2021-07-06 16:51:08 +0530

    parameters and user controllers

--- projbackend/app.js
+++ projbackend/app.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+//my Routes
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 // mongoose.connect("mongodb://localhost:27017/test", {
 //   useNewUrlParser: true,
@@ -31,6 +33,7 @@ app.use(cors());
 
 //My Routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 const { check, validationResult } = require("express-validator");
 
 router.post(
@@ -8,12 +8,29 @@ router.post(
   [
     check("name", "Name should be at least 3 char").isLength({ min: 3 }),
     check("email", "Email is required").isEmail(),
-    check("password", "Password should at least be of 3 char").isLength({
-      min: 3,
+    check("password", "Password is required to signin ").isLength({
+      min: 1,
     }),
   ],
   signup
 );
+
+router.post(
+  "/signin",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password should at least be of 3 char").isLength({
+      min: 3,
+    }),
+  ],
+  signin
+);
+
 router.get("/signout", signout);
+
+// router.get("/testroute", isSignedIn, (req, res) => {
+//   // res.send("A protected route");
+//   res.json(req.auth);
+// });
 
 module.exports = router;
